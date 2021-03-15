b9d39b7f927d3299bdfced94ca98f1bc58ed24e0
jonathanyvargas <Jonathanyvargas@gmail.com>
2021-03-14 23:36:17 -0700

    Added randomized dragons

--- sketch.js
+++ sketch.js
@@ -1,7 +1,42 @@
+// dragons I really like
+let dragons = [{
+  name: "blue-eyes",
+  color: "white"
+}, {
+  name: "red-eyes",
+  color: "black"
+}, {
+  name: "deathwing",
+  color: "black"
+}, {
+  name: "bahamut",
+  color: "black"
+}, {
+  name: "shenron",
+  color: "green"
+}, {
+  name: "rayquaza",
+  color: "green"
+}];
+
+let randomIndex;
+
 function setup() {
   createCanvas(400, 400);
+  background(220);
+
 }
 
 function draw() {
-  background(220);
+
+}
+
+function mousePressed() {
+  backgroun(random(200, 255));
+
+  randomIndex = int(random(dragons.length));
+  text(dragons[randomIndex].name, 50, 50);
+  dragons.splice(randomIndex,1);
+
+
 }
